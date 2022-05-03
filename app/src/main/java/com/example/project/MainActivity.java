9968769ab90a49ddec82ec1ed95a5ffed8a1c9b5
b21oscpe <b21oscpe@student.his.se>
2022-05-03 15:28:14 +0200

    added references to my secondactivity with an onclick and an edittext

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,34 +1,52 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    Button button;
+    TextView text_label;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("==>", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        text_label = findViewById(R.id.text_label);
+
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
 
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Log.d("==>", "Signed in!");
+                Log.d("==>", "Click registered");
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("==>", "onResume");
+        super.onResume();
+
+        String name = preferences.getString("name", "name cant be found");
+        text_label.setText(name);
     }
 }
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,36 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button button;
+    SharedPreferences preferences;
+    EditText edit_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        edit_name = findViewById(R.id.editTextTextPersonName);
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "Registered click");
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("name", edit_name.getText().toString());
+                editor.apply();
+            }
+        });
     }
 }
